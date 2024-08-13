898a2a2eb3a9ac3b3d54e1b4fafe8a5dfd2769a6
Ramon1232 <juramartinezmu@ittepic.edu.mx>
2024-08-13 15:19:29 -0700

    updagte pdf and icon

--- next.config.mjs
+++ next.config.mjs
@@ -4,21 +4,9 @@ const nextConfig = {};
 
 export default {
   nextConfig,
-  // Otras configuraciones de Next.js
   reactStrictMode: true,
-
-  // Configuración para servir fuentes estáticas
   async headers() {
     return [
-      {
-        source : '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "upgrade-insecure-requests",
-          },
-        ],
-      },
       {
         source: '/fonts/:slug*',
         headers: [
